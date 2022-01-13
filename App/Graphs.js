094389d864152ee030f7e951999b0943942d5244
Omar Razzack <oarazzack@ucdavis.edu>
2022-01-13 10:55:25 -0800

    Fixed initialization/ Initialized Graphs component

---
+++ App/Graphs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Graphs extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
+
+export default Graphs
--- App/app.js
+++ App/app.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
+import Graphs from './Graphs.js'
 class App extends React.Component {
 
   render() {
-
+   return(
+     <Graphs />
+   )
   }
 }
 
 
-ReactDOM.render(<App/>, document.getElementByID('app'))
+ReactDOM.render(<App/>, document.getElementById('app'));
