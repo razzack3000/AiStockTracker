7fc23c00c0c14f8cc1d7bc9acb720d85a9038391
Omar Razzack <oarazzack@ucdavis.edu>
2022-01-13 09:09:47 -0800

    Initialized App

---
+++ App/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class App extends React.Component {
+
+  render() {
+
+  }
+}
+
+
+ReactDOM.render(<App/>, document.getElementByID('app'))
