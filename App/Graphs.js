e49917650d0c8dafa4b193def7f25e99753ebc32
Omar Razzack <oarazzack@ucdavis.edu>
2022-01-17 11:23:06 -0800

    Added API

--- App/Graphs.js
+++ App/Graphs.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { fetchStockData } from './api'
+import { polygonClient, restClient, websocketClient } from "@polygon.io/client-js";
+const rest = restClient("a2qwjfHSc4TWL5AML9sbjFaJTiMV1FPI");
 
 class Graphs extends React.Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      data: {},
+      listOfStocks: ['AAPL']
+    }
+    const getData = this.getData.bind(this);
   }
+  getData(stockTicker){
+    rest.stocks.previousClose(stockTicker)
+    .then((stockData) => {
+      //console.log(data)
+      this.setState({
+        data: {...this.state.data, stockTicker: stockData}
+      })
+    })
+  }
+
   render() {
+    for (let ticker in this.state.listOfStocks) {
+      this.getData(ticker)
+    }
     return (
-      <h1>Hello</h1>
+     <h1>{JSON.stringify(this.state.data)}</h1>
+
     )
   }
 }
---
+++ App/api.js
@@ -0,0 +1,23 @@
+
+export function fetchStockData(){
+  return fetch('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2020-10-14?adjusted=true&apiKey=a2qwjfHSc4TWL5AML9sbjFaJTiMV1FPI')
+  // .then((result) => result.json)
+  .then((data) => {
+    return data
+  })
+}
+
+
+
+import { polygonClient, restClient, websocketClient } from "@polygon.io/client-js";
+const rest = restClient("a2qwjfHSc4TWL5AML9sbjFaJTiMV1FPI");
+
+// you can use the api now
+
+rest.stocks
+  .previousClose("C:AAPL")
+  .then((data) => {
+    return data
+  })
+  .catch(/* your error handler*/);
+
